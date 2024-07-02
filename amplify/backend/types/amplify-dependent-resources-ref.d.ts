cc5fd44f166d51468533e067dc7862f528a1f089
naveenb <naveen.bodhula@outlook.com>
2024-07-02 12:03:55 +0530

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,30 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apie89234da": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "amplifyround3": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "hosting": {
+    "S3AndCloudFront": {
+      "CloudFrontDistributionID": "string",
+      "CloudFrontDomainName": "string",
+      "CloudFrontOriginAccessIdentity": "string",
+      "CloudFrontSecureURL": "string",
+      "HostingBucketName": "string",
+      "Region": "string",
+      "S3BucketSecureURL": "string",
+      "WebsiteURL": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+//import Amplify, { API } from 'aws-amplify'
+import { get } from 'aws-amplify/api';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const myAPI = "apie89234da"
+const path = '/customers'; 
+
+const App = () => {
+  const [input, setInput] = useState("")
+  const [customers, setCustomers] = useState([])
+
+  //Function to fetch from our backend and update customers array
+  async function getCustomer(e) {
+    let customerId = e.input
+    const restOperation = get({apiName: myAPI, path: path + "/" + customerId})
+    const { body } = await restOperation.response;
+    const response = await body.json();
+    console.log(response)
+    let newCustomers = [...customers]
+    newCustomers.push(response)
+    setCustomers(newCustomers)
+  }
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Super Simple React App</h1>
+      <div>
+          <input placeholder="customer id" type="text" value={input} onChange={(e) => setInput(e.target.value)}/>      
+      </div>
+      <br/>
+      <button onClick={() => getCustomer({input})}>Get Customer From Backend</button>
+
+      <h2 style={{visibility: customers.length > 0 ? 'visible' : 'hidden' }}>Response</h2>
+      {
+       customers.map((thisCustomer, index) => {
+         return (
+        <div key={thisCustomer.customerId}>
+          <span><b>CustomerId:</b> {thisCustomer.customerId} - <b>CustomerName</b>: {thisCustomer.customerName}</span>
+        </div>)
+       })
+      }
     </div>
-  );
+  )
 }
 
 export default App;
